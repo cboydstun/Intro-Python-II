5948d68a2278204375030a4b71bc125133240dc9
Chris Boydstun <chrisboydstun@gmail.com>
2020-04-10 10:06:53 -0500

    finished MVP: Day 2

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,31 @@
 from room import Room
 from player import Player
-
+from item import Item
+import textwrap
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                    "North of you, the cave mount beckons",
+                    [
+                        Item("Sword", "A steel greatsword"),
+                        Item("Coins", "100 gold coins")
+                    ]),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer",
+                    "Dim light filters in from the south. Dusty passages run north and east."),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook",
+                    "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage",
+                    "The narrow passage bends here from west to north. The smell of gold permeates the air."),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber",
+                    "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
+
+    'sewer': Room("Sewer Entrance",
+                    "You've found a cover to an old sewer entrance, but the path looks dangerous"),
 }
 
 
@@ -30,6 +36,8 @@ room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
 room['overlook'].s_to = room['foyer']
+room['overlook'].e_to = room['sewer']
+room['sewer'].w_to = room['overlook']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
@@ -38,13 +46,29 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
-user = Player(room['outside'])
-play = True
+player = Player(input("What's your name? "), room['outside'])
 
-def end():
-    global play
-    play=False
+full_direction = {
+    "n": "North",
+    "e": "East",
+    "s": "South",
+    "w": "West",
+}
+
+def next_room(dir, current_room):
+    direction = dir + "_to"
+    return getattr(current_room, direction)
+
+def move_player(ply, dir):
+    room = next_room(dir, ply.current_room)
+    if room:
+        ply.current_room = room
+        print(f"You move {full_direction[dir]} and find yourself somewhere new")
+        return True
+    else:
+        return False
+# Make a new player object that is currently in the 'outside' room.
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -55,14 +79,58 @@ def end():
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-while play:
-    print(f"Current Room: {user.currentRoom.name}\n\n{user.currentRoom.desc}")
-    action = input("What would you like to do (q to quit)? Enter a cardinal direction to move in that direction if possible.\n")
-    if len(action)<1:
-        print("No Action")
-    elif 'q' in action.lower():
-        end()
-    else:
-        user.move(action[0].lower())
 
-print("Good Bye")
+def center_wrap(text, cwidth=80, **kw):
+    lines = textwrap.wrap(text, **kw)
+    return "\n".join(line.center(cwidth) for line in lines)
+def center_print(obj):
+    lines = str(obj).splitlines()
+    for line in lines:
+        print(center_wrap(line, cwidth=80, width=50))
+
+done = False
+help_string = """
+Commands:
+    n, e, s, w - to move in a direction specified
+    get/take [ITEM NAME] - pickup item
+    drop [ITEM NAME] - drop item
+    i/inventory - list all items in inventory
+    quit - to quit the game
+    help - to view these commands again
+"""
+print(f"""
+Welcome {player.name}!
+{help_string}
+""")
+
+while not done:
+    center_print(player.current_room)
+    commands = input("Where would you like to go? ").strip().lower().split(" ")
+    if commands[0] in ["n", "e", "s", "w"]:
+        if move_player(player, commands[0]):
+            continue
+        else:
+            print("There is no room that way")
+            continue
+    elif commands[0] in ["take", "give"]:
+        inv = player.pickup_item(commands[1])
+        if not inv:
+            print(f"You tried to find a {commands[1]}, but there was none to be found")
+            continue
+    elif commands[0] == "drop":
+        item = player.drop_item(commands[1])
+        if not item:
+            print(f"You don't have a {commands[1]} on you")
+            continue
+    elif commands[0] in ["i", "inventory"]:
+        center_print(player.inventory_string())
+        continue
+    elif commands[0] == "quit":
+        done = True
+        continue
+    elif commands[0] == "help":
+        print(help_string)
+        continue
+    else:
+        print("Invalid command")
+        continue
--- src/player.py
+++ src/player.py
@@ -1,23 +1,42 @@
-class Player(object):
-    def __init__(self, room=None):
-        self.currentRoom=room
-        self.inv=[]
-        self.directions={
-            "n":self.currentRoom.n_to,
-            "s":self.currentRoom.s_to,
-            "e":self.currentRoom.e_to,
-            "w":self.currentRoom.w_to
-        }
+# Write a class to hold player information, e.g. what room they are in
+# currently.
 
-    def move(self, direction):
-        if self.directions.get(direction,'Improper Action') != None:
-            self.currentRoom=self.directions[direction]
-            self.directions={
-                "n":self.currentRoom.n_to,
-                "s":self.currentRoom.s_to,
-                "e":self.currentRoom.e_to,
-                "w":self.currentRoom.w_to
-            }
-            print(f"Moved to the {self.currentRoom.name}")
-        else:
-            print("No room there") 
+class Player:
+  def __init__(self, name, current_room, inventory = []):
+    self.current_room = current_room
+    self.name = name
+    self.inventory = inventory
+
+  def __add_item__(self, item):
+    self.inventory.append(item)
+    item.on_take()
+    return self.inventory
+
+  def __remove_item__(self, idx):
+    item = self.inventory.pop(idx)
+    item.on_drop()
+    return self.inventory
+
+  def pickup_item(self, name):
+    item = self.current_room.remove_item(name)
+    if item:
+      return self.__add_item__(item)
+    else:
+      return False
+
+  def drop_item(self, name):
+    item = None
+    item_index = None
+    for idx, i in enumerate(self.inventory):
+      if i.name.lower() == name:
+        item = i
+        item_index = idx
+        break
+    if item:
+      self.current_room.add_item(item)
+      return self.__remove_item__(item_index)
+    else:
+      return False
+
+  def inventory_string(self):
+    return "\n".join(str(item) for item in self.inventory)
